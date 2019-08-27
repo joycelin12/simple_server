6fed26977f723ad67c5afd81520b450370d4e91a
joycelin12 <joycelin12@hotmail.com>
2019-08-27 18:37:39 -0400

    Simple routing in web server adding timeout too

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 "use strict";
 
+const routeResponseMap = {
+	
+	"/info" : "<h1>Info Page</h1>",
+	"/contact" : "<h1>Contact Us</h1>",
+	"/about" : "<h1>Learn More About Us </h1>",
+	"/hello" : "<h1>Say hello by emaling us here</h1>",
+	"/error" : "<h1>Sorry the page you are looking for is not here.</h1>"
+};
+
 const port = 3000, //require the http and http-status-codes modules
 	http = require("http"),
 	httpStatus = require("http-status-codes"),
-	app = http.createServer((request, response) => { //Create the server with request and server parameters
+	app = http.createServer((req, res) => { //Create the server with request and server parameters
 		console.log("Received an incoming request!");
-		response.writeHead(httpStatus.OK, {
+		res.writeHead(httpStatus.OK, {
                  "Content-Type": "text/html"
 		}); // write the response to the client.
-                
-		let responseMessage = "<h1>Hello, Universe!</h1>";
-		response.write(responseMessage);
-                response.end();
-		console.log(`Sent a response : ${responseMessage}`);
 
+		if(routeResponseMap[req.url]) {
+
+                    setTimeout(() => res.end(routeResponseMap[req.url]), 2000); //Wrap a request with setTimeout to delay the response manually
+		} else {
+		    res.end("<h1>Welcome!</h1>")	
+  
+		}
+                
+	
 	});
 
 app.listen(port); //tell the application server to listen on port 3000
